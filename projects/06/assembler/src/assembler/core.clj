cdcd958777b58ccc5116e5180fc2e811b3002502
giulio <giuliop@gmail.com>
2017-10-21 20:26:30 +0200

    working on it

--- projects/06/assembler/src/assembler/core.clj
+++ projects/06/assembler/src/assembler/core.clj
@@ -1,37 +1,13 @@
 (ns assembler.core
   (:require [clojure.string :as str]
             [assembler.parse :as parse]
+            [assembler.table :as table]
             [clojure.test :refer :all]))
-
-(def predefined-table
-  {"R0" 0
-   "R1" 1
-   "R2" 2
-   "R3" 3
-   "R4" 4
-   "R5" 5
-   "R6" 6
-   "R7" 7
-   "R8" 8
-   "R9" 9
-   "R10" 10
-   "R11" 11
-   "R12" 12
-   "R13" 13
-   "R14" 14
-   "R15" 15
-   "SP" 0
-   "LCL" 1
-   "ARG" 2
-   "THIS" 3
-   "THAT" 4
-   "SCREEN" 16384
-   "KEYBOARD" 24576})
 
 (defn add-labels [processed-lines]
   "Takes asm processed lines and returns an augmented predefined table with the
   labels added"
-  (let [acc {:table predefined-table, :line-num 0}
+  (let [acc {:table table/predefined, :line-num 0}
         f (fn [acc line]
             (if-let [label (:label line)]
               (assoc-in acc [:table label] (:line-num acc))
@@ -43,14 +19,38 @@
 (defn pre-process [asm]
   "Take an asm program, removes whitespaces/comments, create a symbol table adding
   labels to the predefined table; return [processed-lines, table]"
-  (let [processed-lines (map parse/labels (str/split-lines asm))
+  (let [processed-lines (map parse/tokenize (str/split-lines asm))
         table (add-labels processed-lines)]
     [(remove parse/is-empty processed-lines) table]))
+
+(defn to-binary-16 [n]
+  "Takes a decimal n and returns its 16-bit representation as a string; it assumes
+  the number n fits in 16 bits"
+  (let [b (Integer/toBinaryString n)
+        pad (- 16 (count b))]
+    (str (apply str (repeat pad "0")) b)))
+
+(defn a-line-to-binary [line table]
+"Takes an pre-prcessed a-instruction and converts it to binary with the input table"
+  (if (not-empty (:a-value line)) (to-binary-16 (:a-value line))
+    (to-binary-16 (get table (:a-var line)))))
+
+(defn c-line-to-binary [line]
+  (str (get table/comp-codes (:comp line))
+       (get table/dest-codes (:dest line))
+       (get table/jump-codes (:jump line))))
+
+(defn line-to-binary-with [table]
+  "Returns a function that takes a pre-processed asm line and translates it to
+  binary with the supplied table"
+  (fn [line]
+    (if (parse/is-a-instruction line) (a-line-to-binary line table)
+      (c-line-to-binary line))))
 
 (defn translate [pre-processed-lines table]
   "Take an asm program split into lines with whitespaces already removed and
   a symbol table with labels already added and translate it to a binary program"
-  )
+  (map (line-to-binary-with table) pre-processed-lines))
 
 (defn name-file [file]
   "Change the suffix of file to .hack"
@@ -58,12 +58,17 @@
     (if (nil? radix) (str file ".hack")
       (str radix "hack"))))
 
+(defn write-file [file lines]
+  "Take a filename and a seq of lines and writes them to disk"
+  (with-open [w (clojure.java.writer file)]
+    (doseq [line lines] (.write w line) (.newLine w))))
+
 (defn assemble [file]
   "Take an input assembly file .asm and output the assembled binary code in a .hack
   file"
   (let [asm (slurp file)
         [pre-processed-asm, table] (pre-process asm)]
-    (spit (name-file file) (translate pre-processed-asm table))))
+    (write-file (name-file file) (translate pre-processed-asm table))))
 
 ;;; TESTING ;;;
 (deftest name-file-test
@@ -73,7 +78,7 @@
 )
 
 (deftest pre-process-test
-  (let [table (assoc predefined-table "OUTPUT_FIRST" 10
+  (let [table (assoc table/predefined "OUTPUT_FIRST" 10
                                       "OUTPUT_D" 12
                                       "INFINITE_LOOP" 14)
         asm (slurp "src/assembler/test.asm")
--- projects/06/assembler/src/assembler/parse.clj
+++ projects/06/assembler/src/assembler/parse.clj
@@ -1,8 +1,12 @@
 (ns assembler.parse
   (:require [clojure.string :as str]))
 
-(def re-a-instr #"^@([a-zA-Z_.$:][a-zA-Z_.$:\d]*)")
 (def re-label #"^\((\S*)\)")
+(def re-a-var #"^@([a-zA-Z_.$:][a-zA-Z_.$:\d]*)")
+(def re-a-value #"^@(\d*)")
+(def re-dest #"^[AMD]+(?==)")
+(def re-jump #"((?<=;)J[GELNM][TQEP])")
+(def re-comp #"(?:.*=)?([.^;]+).*")
 
 (defn remove-whitespace [line]
   "Takes a line, removes comments and leading/trailing whitespaces"
@@ -11,19 +15,36 @@
        (str "")
        (str/trim)))
 
-(defn labels [line]
+(defn tokenize [line]
   "Takes a line and returns a map with the following fields:
   :label (a string with the label or nil if the line has no label)
-  :instr (a string with the instruction with comments removed or nil if empty)
+  :instr (a string with the instruction with comments removed or empty if a label)
+  :type ('a' for a instruction or 'c' for c instruction)
+  :a-value (a string with the decimal value of the a instruction if there or empty)
+  :a-var (a string with the var name of the a instruction if there or empty)
+  :comp (a string with the comp field of the c instruction if there or empty)
+  :dest (a string with the dest field of the c instruction if there or empty)
+  :jump (a string with the jump field of the c instruction if there or empty)
   "
   (let [line (remove-whitespace line)
         label (last (re-find re-label line))
-        instr (if label "" line)]
+        i-type (if (= '@' (first line)) 'a' 'c') ]
     {:label label
-     :instr instr
+     :instr (if label "" line)
+     :type i-type
+     :a-value (last (re-find re-a-value line))
+     :a-var (last (re-find re-a-var line))
+     :comp (if (= 'a'i-type) "" (last (re-find re-comp line)))
+     :dest (if (= 'a'i-type) "" (last (re-find re-dest line)))
+     :jump (if (= 'a'i-type) "" (last (re-find re-jump line)))
      }))
 
 (defn is-empty [pre-processed-line]
   "Returns true if the pre-processed line is empty"
   (empty? (:instr pre-processed-line)))
 
+(defn is-a-instruction [pre-processed-line]
+  (= 'a' (:type pre-processed-line)))
+
+(defn is-c-instruction [pre-processed-line]
+  (= 'c' (:type pre-processed-line)))
