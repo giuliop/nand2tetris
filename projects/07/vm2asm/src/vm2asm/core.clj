caadc263ed2f3b4cab9ff6c7e2ddf8ec135f639f
giulio <giuliop@gmail.com>
2017-10-29 18:39:21 +0100

    all tests passed!

--- projects/07/vm2asm/src/vm2asm/core.clj
+++ projects/07/vm2asm/src/vm2asm/core.clj
@@ -13,35 +13,46 @@
         ;f (fn [acc line] ())]
     ;(first (reduce f acc vm-lines))))
 
-(defn translate-f [vm-line]
+(defn translate-f [filename vm-line]
   "Takes a vm string and returns its translation into an asm string"
   (let [{:keys [cmd arg1 arg2]} (parse/tokenize vm-line)
         cmd-f (get table/commands cmd)]
-    (cond arg2 (cmd-f arg1 arg2)
+    (cond arg2 (cmd-f arg1 arg2 filename)
           arg1 (cmd-f arg1)
           :else (cmd-f))))
 
-(defn translate-to-asm [vm-lines]
-  (conj (map translate-f vm-lines) table/init))
+(defn translate-to-asm [filename vm-lines]
+  (conj (map (partial translate-f filename) vm-lines) table/init))
 
-(defn name-file [file]
+(defn rename-to-asm [filename]
   "Change the suffix of file to .asm"
-  (let [radix (re-find #".*\." file)]
-    (if (nil? radix) (str file ".asm")
+  (let [radix (re-find #".*\." filename)]
+    (if (nil? radix) (str filename ".asm")
       (str radix "asm"))))
 
-(defn write-file [file lines]
+(defn write-file [filename lines]
   "Takes a filename and a seq of lines and writes them to disk"
   ;lines)
-  (with-open [w (clojure.java.io/writer file)]
+  (with-open [w (clojure.java.io/writer filename)]
     (doseq [line lines
           :when (not (empty? line))]
       (.write w line))))
 
-(defn vm-to-asm [file]
-  "Takes a .vm file and outputs the assembly code in a same name .asm file"
-  (->> (slurp file)
-       (str/split-lines)
-       (translate-to-asm)
-       (write-file (name-file file))))
+(defn radix [filename]
+  "Takes a filename and remove directory path and extension"
+  (let [radix (last (re-matches #"^(?:.*/)?(.*?)(?:\.[^.]*$)?" filename))]
+    radix))
 
+(defn vm-to-asm [filename]
+  "Takes a .vm file and outputs the assembly code in a same name .asm file"
+  (->> (slurp filename)
+       (str/split-lines)
+       (translate-to-asm (radix filename))
+       (write-file (rename-to-asm filename))))
+
+(deftest radix-test
+  (is (= "radix" (radix "radix")))
+  (is (= "radix" (radix "../dir1/dir2/dir3/radix")))
+  (is (= "radix" (radix "../dir1/dir2/dir3/radix.")))
+  (is (= "radix" (radix "../dir1/dir2/dir3/radix.ext")))
+  (is (= "radix.notext" (radix "../dir1/dir2/dir3/radix.notext.ext"))))
--- projects/07/vm2asm/src/vm2asm/table.clj
+++ projects/07/vm2asm/src/vm2asm/table.clj
@@ -16,30 +16,89 @@
                  "M=D" "\n"
                  SP++))
 
-(defn POP- [dest]
+(defn POP-DA [D-or-A]
   (str SP--
        "@SP" "\n"
        "A=M" "\n"
-       dest "=M" "\n"))
+       D-or-A "=M" "\n"))
 
 (defn LOAD-D [n]
   (str "@" n "\n"
        "D=A" "\n"))
 
-(defn push [arg1 arg2]
+(defn POP- [reg arg2]
+  (str (LOAD-D arg2)
+       "@" reg "\n"
+       (when-not (= reg "R5") (str "A=M" "\n")) ; no indirection for TEMP register
+       "D=D+A" "\n"
+       "@R13" "\n"
+       "M=D" "\n"       ; R13 holds the address to pop to
+       (POP-DA "D")
+       "@R13" "\n"
+       "A=M" "\n"
+       "M=D" "\n"))
+
+(defn POP-POINTER [arg2]
+  (let [reg (case arg2 "0" "THIS", "1" "THAT")]
+    (str (POP-DA "D")
+         "@" reg "\n"
+         "M=D" "\n")))
+
+(defn POP-STATIC [arg2 filename]
+  (str (POP-DA "D")
+       "@" filename "." arg2 "\n"
+       "M=D" "\n"))
+
+(defn pop- [arg1 arg2 filename]
   (case arg1
-    "constant" (str (LOAD-D arg2)
-                    PUSH-D)
+    "argument" (POP- "ARG" arg2)
+    "local" (POP- "LCL" arg2)
+    "this" (POP- "THIS" arg2)
+    "that" (POP- "THAT" arg2)
+    "temp" (POP- "R5" arg2)
+    "pointer" (POP-POINTER arg2)
+    "static" (POP-STATIC arg2 filename)
+    ))
+
+(defn PUSH- [reg arg2]
+  (str (LOAD-D arg2)
+       "@" reg "\n"
+       (when-not (= reg "R5") (str "A=M" "\n")) ; no indirection for TEMP register
+       "A=D+A" "\n"
+       "D=M" "\n"
+       PUSH-D))
+
+(defn PUSH-POINTER [arg2]
+  (let [reg (case arg2 "0" "THIS", "1" "THAT")]
+    (str "@" reg "\n"
+         "D=M" "\n"
+         PUSH-D)))
+
+(defn PUSH-STATIC [arg2 filename]
+  (str "@" filename "." arg2 "\n"
+       "D=M" "\n"
+       PUSH-D))
+
+(defn push [arg1 arg2 filename]
+  (case arg1
+    "constant" (str (LOAD-D arg2) PUSH-D)
+    "argument" (PUSH- "ARG" arg2)
+    "local" (PUSH- "LCL" arg2)
+    "this" (PUSH- "THIS" arg2)
+    "that" (PUSH- "THAT" arg2)
+    "temp" (PUSH- "R5" arg2)
+    "pointer" (PUSH-POINTER arg2)
+    "static" (PUSH-STATIC arg2 filename)
     ))
 
 (defn binary-op [op]
-  (fn [] (str (POP- "D")
-              (POP- "A")
+  (fn [] (str (POP-DA "D")
+              (POP-DA "A")
               "D=A" op "D" "\n"
               PUSH-D)))
 
 (defn unary-op [op]
-  (fn [] (str (POP- "D")
+  (fn [] (str (POP-DA "D")
        "D=" op "D" "\n"
        PUSH-D)))
 
@@ -50,7 +109,7 @@
                         ">" "JGT")
                true-label (gensym "true")
                continue-label (gensym "continue")]
-           (str (POP- "D")
+           (str (POP-DA "D")
                 (POP- "A")
                 "D=A-D" "\n"
                 "@" true-label "\n"
@@ -77,6 +136,4 @@
                "lt" (test-op "<")
                nil #(str)
                })
-
-
 
